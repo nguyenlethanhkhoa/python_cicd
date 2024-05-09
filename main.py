fd967b883bfae2379cec783dcbb694896699cfaf
Khoa Nguyen <khoa.nguyenlethanh@gmail.com>
2024-05-09 18:58:50 +0700

    format main.py to black

--- main.py
+++ main.py
@@ -3,17 +3,14 @@ import os
 
 def _sum(*args):
     total = 0
-
     for arg in args:
         total += arg
-
     return total
 
 
 def get_service_name():
-
-    return os.environ.get('SERVICE_NAME')
+    return os.environ.get("SERVICE_NAME")
 
 
 def get_port():
-    return int(os.environ.get('PORT'))
+    return int(os.environ.get("PORT"))
