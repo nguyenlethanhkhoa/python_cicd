80ec3c5545a3d0d1f6bf3b185df7b35adfeed1bb
Khoa Nguyen <khoa.nguyenlethanh@gmail.com>
2024-05-11 16:24:06 +0700

    test black linter action

--- .github/actions/black_linter/entrypoint.sh
+++ .github/actions/black_linter/entrypoint.sh
@@ -46,9 +46,6 @@ done
 black_exit_val="0"
 echo "[action-black] ${black_print_str} python code using the black formatter..."
 # shellcheck disable=SC2086
-black_script="/venv/bin/black ${black_args_tmp[*]} 2>&1"
-echo ' ----------------------- BLACK SCRIPT ----------------------------'
-echo $black_script
 black_output="$(/venv/bin/black ${black_args_tmp[*]} 2>&1)" || black_exit_val="$?"
 if [[ "${quiet}" != 'true' ]]; then
   echo "${black_output}"
