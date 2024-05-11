3eac85d4b2658e053daadd7dbcf2a434fa112384
Khoa Nguyen <khoa.nguyenlethanh@gmail.com>
2024-05-11 16:48:37 +0700

    update

--- .github/actions/black_linter/entrypoint.sh
+++ .github/actions/black_linter/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo "------------------ LL COMMAND ------------------------------"
+ls -l
+
+echo "------------------ BLACK COMMAND ------------------------------"
 /venv/bin/black . --check --diff
